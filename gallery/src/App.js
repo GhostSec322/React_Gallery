074b7e859fadc6a6aa3c8a616ffa87a09d88be46
GhostSec322 <GhostSec322@gmail.com>
2023-10-15 21:32:36 +0900

    2023-10-15 pixabay api 연동

---
+++ gallery/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Pixabay from './Component/Pixabay';
+
+function App() {
+  return (
+    <div className="App">
+      <Pixabay />
+    </div>
+  );
+}
+
+export default App;

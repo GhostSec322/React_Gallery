d95d73969f2050b28ad29db65f6a40af8edc6c23
GhostSec322 <GhostSec322@gmail.com>
2023-11-04 04:19:11 +0900

    기존 무한로딩 현상, 로그인로그아웃 기능 수정완료

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,11 +1,21 @@
+// App.js
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Component/Home';
+import Login from './Component/Login';
 import Pixabay from './Component/Pixabay';
 
 function App() {
   return (
-    <div className="App">
-      <Pixabay />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Pixabay />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

130697c5728f2bdc0a08069b11bc7c91ac1c1ddd
GhostSec322 <GhostSec322@gmail.com>
2023-11-25 00:04:45 +0900

    Redux 처리 완료

---
+++ gallery/src/Component/Upload.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from 'react';
+import { collection, getDocs, doc, getDoc, updateDoc, setDoc } from 'firebase/firestore';
+import { getStorage, ref } from 'firebase/storage';
+import { auth, db } from './config';
+
+function Upload(){
+
+}
+export default Upload

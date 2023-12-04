0fc0ca0d035e2963328f4cef4937576c990edb1e
GhostSec322 <GhostSec322@gmail.com>
2023-12-05 02:08:59 +0900

    마이페이지 컴포넌트 제작 카테고리 삭제 기능 구현

--- gallery/src/App.js
+++ gallery/src/App.js
@@ -1,10 +1,10 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Component/Home';
-import Login from './Component/Login';
-import Pixabay from './Component/Pixabay';
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import Login from "./Component/Login";
+import Pixabay from "./Component/Pixabay";
+import Mypage from "./Component/Page/Mypage";
 function App() {
   return (
     <Router>
@@ -13,6 +13,7 @@ function App() {
           <Route path="/" element={<Pixabay />} />
           <Route path="/home" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/Mypage" element={<Mypage />} />
         </Routes>
       </div>
     </Router>
--- gallery/src/Component/store.js
+++ gallery/src/Component/store.js
@@ -1,32 +1,49 @@
-// store.js
+import { createStore } from "redux";
 
-import { createStore } from 'redux';
+// 액션 타입
+const UPDATE_KEY_VALUE = "UPDATE_KEY_VALUE";
+const READ_KEY_VALUE = "READ_KEY_VALUE";
 
-// 초기 상태 설정
+// 초기 상태
 const initialState = {
-  keyValueDict: {}, // key:value 쌍을 저장할 객체
+  // 로컬 스토리지에서 keyValueDict를 가져와서 초기 상태로 설정
+  keyValueDict: JSON.parse(localStorage.getItem("keyValueDict")) || {},
 };
 
-// 액션 타입 정의
-const UPDATE_KEY_VALUE = 'UPDATE_KEY_VALUE';
-
-// 액션 생성자 함수
+// 액션 생성자
 export const updateKeyValue = (key, value) => ({
-    type: UPDATE_KEY_VALUE,
+  type: UPDATE_KEY_VALUE,
   payload: { key, value },
-  
+});
+
+// 새로운 액션 생성자 추가
+export const readKeyValue = (keyToRead) => ({
+  type: READ_KEY_VALUE,
+  payload: { keyToRead },
 });
 
 // 리듀서 함수
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_KEY_VALUE:
+      // 새로운 키-값 쌍을 추가하고 로컬 스토리지에 상태 저장
+      const { key, value } = action.payload;
+      const updatedKeyValueDict = {
+        ...state.keyValueDict,
+        [key]: value,
+      };
+      localStorage.setItem("keyValueDict", JSON.stringify(updatedKeyValueDict));
       return {
         ...state,
-        keyValueDict: {
-          ...state.keyValueDict,
-          [action.payload.key]: action.payload.value,
-        },
+        keyValueDict: updatedKeyValueDict,
+      };
+    case READ_KEY_VALUE:
+      // 해당 키의 값을 반환하거나 초기 상태 반환
+      const { keyToRead } = action.payload;
+      const valueToRead = state.keyValueDict[keyToRead] || null;
+      return {
+        ...state,
+        valueToRead,
       };
     default:
       return state;
